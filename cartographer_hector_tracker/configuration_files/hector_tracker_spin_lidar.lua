2ee467f922caedd63d9333697ddf532d8bff1429
Stefan Kohlbrecher <kohlbrecher@sim.tu-darmstadt.de>
2016-10-24 18:01:18 +0200

    Changes required by current cartographer version. demo with bagfile works now.

--- cartographer_hector_tracker/configuration_files/hector_tracker_spin_lidar.lua
+++ cartographer_hector_tracker/configuration_files/hector_tracker_spin_lidar.lua
@@ -1,4 +1,4 @@
--- Copyright 2016 Stefan Kohlbrecher, TU Darmstadt
+-- Copyright 2016 The Cartographer Authors
 --
 -- Licensed under the Apache License, Version 2.0 (the "License");
 -- you may not use this file except in compliance with the License.
@@ -12,34 +12,43 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
-include "trajectory_builder_3d.lua"
-include "sparse_pose_graph.lua"
+include "map_builder.lua"
 
 options = {
-  sparse_pose_graph = SPARSE_POSE_GRAPH,
-  trajectory_builder = TRAJECTORY_BUILDER_3D,
+  map_builder = MAP_BUILDER,
+  sensor_bridge = {
+    horizontal_laser_min_range = 0.6,
+    horizontal_laser_max_range = 30.,
+    horizontal_laser_missing_echo_ray_length = 1.,
+    constant_odometry_translational_variance = 1e-7,
+    constant_odometry_rotational_variance = 1e-7,
+  },
   map_frame = "map",
-  odom_frame = "odom",
   tracking_frame = "base_link",
+  published_frame = "odom",
+  odom_frame = "odom",
   provide_odom_frame = false,
-  expect_odometry_data = true,
-  laser_min_range = 0.6,
-  laser_max_range = 30.,
-  laser_missing_echo_ray_length = 1.,
-  num_lasers_3d = 1
+  use_odometry_data = true,
+  use_horizontal_laser = false,
+  use_horizontal_multi_echo_laser = false,
+  num_lasers_3d = 1,
+  lookup_transform_timeout_sec = 0.2,
+  submap_publish_period_sec = 0.3,
+  pose_publish_period_sec = 5e-3,
 }
 
-options.sparse_pose_graph.optimize_every_n_scans = 320
-options.sparse_pose_graph.constraint_builder.sampling_ratio = 0.03
-options.sparse_pose_graph.optimization_problem.ceres_solver_options.max_num_iterations = 10
+TRAJECTORY_BUILDER_3D.scans_per_accumulation = 180
+
+MAP_BUILDER.use_trajectory_builder_3d = true
+MAP_BUILDER.num_background_threads = 7
+MAP_BUILDER.sparse_pose_graph.optimization_problem.huber_scale = 5e2
+MAP_BUILDER.sparse_pose_graph.optimize_every_n_scans = 320
+MAP_BUILDER.sparse_pose_graph.constraint_builder.sampling_ratio = 0.03
+MAP_BUILDER.sparse_pose_graph.optimization_problem.ceres_solver_options.max_num_iterations = 10
 -- Reuse the coarser 3D voxel filter to speed up the computation of loop closure
 -- constraints.
-options.sparse_pose_graph.constraint_builder.adaptive_voxel_filter = TRAJECTORY_BUILDER_3D.high_resolution_adaptive_voxel_filter
-options.sparse_pose_graph.constraint_builder.min_score = 0.62
-options.sparse_pose_graph.constraint_builder.log_matches = true
-
-
--- Set this depending on the LIDAR rotation rate
-options.trajectory_builder.scans_per_accumulation = 180
+MAP_BUILDER.sparse_pose_graph.constraint_builder.adaptive_voxel_filter = TRAJECTORY_BUILDER_3D.high_resolution_adaptive_voxel_filter
+MAP_BUILDER.sparse_pose_graph.constraint_builder.min_score = 0.62
+MAP_BUILDER.sparse_pose_graph.constraint_builder.log_matches = true
 
 return options
