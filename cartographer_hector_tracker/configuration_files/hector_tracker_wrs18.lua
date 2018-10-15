8c7f21ae826edbcd2b2551295ff098452af145a0
Stefan Kohlbrecher (Tracker) <kohlbrecher@sim.tu-darmstadt.de>
2018-10-15 11:59:31 +0200

    Update tacker config for WRS.

--- cartographer_hector_tracker/configuration_files/hector_tracker_wrs18.lua
+++ cartographer_hector_tracker/configuration_files/hector_tracker_wrs18.lua
@@ -66,7 +66,7 @@ POSE_GRAPH.optimization_problem.log_solver_summary = true
 
 
 TRAJECTORY_BUILDER_3D.num_accumulated_range_data = 8
-TRAJECTORY_BUILDER_3D.min_range = 0.3
+TRAJECTORY_BUILDER_3D.min_range = 0.7
 TRAJECTORY_BUILDER_3D.max_range = 40
 TRAJECTORY_BUILDER_3D.ceres_scan_matcher.ceres_solver_options.max_num_iterations = 200
 TRAJECTORY_BUILDER_3D.ceres_scan_matcher.rotation_weight = 5
@@ -74,17 +74,17 @@ TRAJECTORY_BUILDER_3D.voxel_filter_size = 0.1
 TRAJECTORY_BUILDER_3D.submaps.high_resolution = 0.1
 
 MAP_BUILDER.use_trajectory_builder_3d = true
-MAP_BUILDER.num_background_threads = 7
+MAP_BUILDER.num_background_threads = 4
 
 TRAJECTORY_BUILDER_3D.submaps.num_range_data = 400
-POSE_GRAPH.constraint_builder.sampling_ratio = 0.3
+POSE_GRAPH.constraint_builder.sampling_ratio = 0.1
 POSE_GRAPH.global_sampling_ratio = 1.0
 POSE_GRAPH.optimize_every_n_nodes = 400
-POSE_GRAPH.constraint_builder.min_score = 0.6
+POSE_GRAPH.constraint_builder.min_score = 0.8
 POSE_GRAPH.log_residual_histograms = false
 
-TRAJECTORY_BUILDER_3D.optimizing_local_trajectory_builder.high_resolution_grid_weight = 5.
-TRAJECTORY_BUILDER_3D.optimizing_local_trajectory_builder.low_resolution_grid_weight = 15.
+TRAJECTORY_BUILDER_3D.optimizing_local_trajectory_builder.high_resolution_grid_weight = 15.
+TRAJECTORY_BUILDER_3D.optimizing_local_trajectory_builder.low_resolution_grid_weight = 45.
 TRAJECTORY_BUILDER_3D.optimizing_local_trajectory_builder.velocity_weight = 8e1
 TRAJECTORY_BUILDER_3D.optimizing_local_trajectory_builder.translation_weight = 1e3
 TRAJECTORY_BUILDER_3D.optimizing_local_trajectory_builder.rotation_weight = 2e2
@@ -98,10 +98,10 @@ TRAJECTORY_BUILDER_3D.optimizing_local_trajectory_builder.scans_per_optimization
 --POSE_GRAPH.optimization_problem.rotation_weight = 1e-10 --3e5
 POSE_GRAPH.optimization_problem.fix_z_in_3d = false
 
-TRAJECTORY_BUILDER_3D.motion_filter.max_distance_meters = 0
+--TRAJECTORY_BUILDER_3D.motion_filter.max_distance_meters = 0
 POSE_GRAPH.optimization_problem.use_online_imu_extrinsics_in_3d = false
 
-POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher_3d.linear_xy_search_window = 1.5
+--POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher_3d.linear_xy_search_window = 1.5
 
 
 
