44b1a8c18cf6fdea9d727fdfac69f7a228301c1a
Kevin_Daun <daun@sim.informatik.tu-darmstadt.de>
2020-01-02 16:15:32 +0100

    Add telemax tsdf config.

--- cartographer_hector_tracker/configuration_files/drz_tracker_enrich19.lua
+++ cartographer_hector_tracker/configuration_files/drz_tracker_enrich19.lua
@@ -24,19 +24,19 @@ options = {
   published_frame = "odom",
   matched_pointcloud_frame = "spin_lidar_lidar_mount_link_fixed",
   odom_frame = "odom",
-  provide_odom_frame = false,  
-  publish_frame_projected_to_2d = false,  
+  provide_odom_frame = false,
+  publish_frame_projected_to_2d = false,
   use_pose_extrapolator = false,
-  use_odometry = false,  
+  use_odometry = true,
   use_nav_sat = false,
   use_landmarks = false,
-  num_laser_scans = 0,  
+  num_laser_scans = 0,
   num_multi_echo_laser_scans = 0,
   num_subdivisions_per_laser_scan = 1,
   num_point_clouds = 1,
   lookup_transform_timeout_sec = 0.2,
   submap_publish_period_sec = 0.1,
-  pose_publish_period_sec = 1e-3,    
+  pose_publish_period_sec = 1e-3,
   trajectory_publish_period_sec = 30e-3,
   rangefinder_sampling_ratio = 1.,
   odometry_sampling_ratio = 1.,
@@ -77,26 +77,32 @@ TRAJECTORY_BUILDER_3D.submaps.high_resolution = 0.05
 MAP_BUILDER.use_trajectory_builder_3d = true
 
 
-TRAJECTORY_BUILDER_3D.submaps.num_range_data = 1E10
-POSE_GRAPH.constraint_builder.sampling_ratio = 1E-10
-POSE_GRAPH.global_sampling_ratio = 1E-10
-POSE_GRAPH.optimize_every_n_nodes = 1E10
-POSE_GRAPH.constraint_builder.min_score = 10
+TRAJECTORY_BUILDER_3D.submaps.num_range_data = 100
+POSE_GRAPH.constraint_builder.sampling_ratio = 0.1
+POSE_GRAPH.global_sampling_ratio = 0.0001
+POSE_GRAPH.optimize_every_n_nodes = 100
+POSE_GRAPH.constraint_builder.min_score = 0.93
 POSE_GRAPH.log_residual_histograms = false
 
-TRAJECTORY_BUILDER_3D.submaps.range_data_inserter.hit_probability = 0.58
-TRAJECTORY_BUILDER_3D.submaps.range_data_inserter.miss_probability = 0.48
+TRAJECTORY_BUILDER_3D.submaps.range_data_inserter.probability_grid_range_data_inserter.hit_probability = 0.58
+TRAJECTORY_BUILDER_3D.submaps.range_data_inserter.probability_grid_range_data_inserter.miss_probability = 0.48
 
-TRAJECTORY_BUILDER_3D.optimizing_local_trajectory_builder.high_resolution_grid_weight = 10.
-TRAJECTORY_BUILDER_3D.optimizing_local_trajectory_builder.low_resolution_grid_weight = 30.
+TRAJECTORY_BUILDER_3D.optimizing_local_trajectory_builder.high_resolution_grid_weight = 60.
+TRAJECTORY_BUILDER_3D.optimizing_local_trajectory_builder.low_resolution_grid_weight = 0.
 TRAJECTORY_BUILDER_3D.optimizing_local_trajectory_builder.velocity_weight = 8e1
 TRAJECTORY_BUILDER_3D.optimizing_local_trajectory_builder.translation_weight = 1e2
-TRAJECTORY_BUILDER_3D.optimizing_local_trajectory_builder.rotation_weight = 2e2
+TRAJECTORY_BUILDER_3D.optimizing_local_trajectory_builder.rotation_weight = 5e1 --2e2
 TRAJECTORY_BUILDER_3D.optimizing_local_trajectory_builder.odometry_translation_weight = 1e2
 TRAJECTORY_BUILDER_3D.optimizing_local_trajectory_builder.odometry_rotation_weight = 1e2
-TRAJECTORY_BUILDER_3D.optimizing_local_trajectory_builder.scans_per_optimization_update = 1 
+TRAJECTORY_BUILDER_3D.optimizing_local_trajectory_builder.scans_per_optimization_update = 1
+TRAJECTORY_BUILDER_3D.optimizing_local_trajectory_builder.initialize_map_orientation_with_imu = false
+TRAJECTORY_BUILDER_3D.optimizing_local_trajectory_builder.calibrate_imu = true
 
 
+TRAJECTORY_BUILDER_3D.submaps.range_data_inserter.range_data_inserter_type = "TSDF_INSERTER_3D"
+TRAJECTORY_BUILDER_3D.submaps.range_data_inserter.tsdf_range_data_inserter.truncation_distance = 0.3
+TRAJECTORY_BUILDER_3D.submaps.range_data_inserter.tsdf_range_data_inserter.num_free_space_voxels = 0
+TRAJECTORY_BUILDER_3D.submaps.grid_type = "TSDF"
 
 --POSE_GRAPH.optimization_problem.acceleration_weight = 1e-10
 --POSE_GRAPH.optimization_problem.rotation_weight = 1e-10 --3e5
